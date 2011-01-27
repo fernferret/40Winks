b7832d5affb48ba6d3b577d631d3452518e36966
Eric Stokes <fernferret@gmail.com>
2011-01-27 17:08:31 -0500

    Drawer Spelling, Image changes

--- src/com/fernferret/fourtywinks/FourtyWinks.java
+++ src/com/fernferret/fourtywinks/FourtyWinks.java
@@ -1,13 +1,43 @@
 package com.fernferret.fourtywinks;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SlidingDrawer;
+import android.widget.SlidingDrawer.OnDrawerCloseListener;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
 
 public class FourtyWinks extends Activity {
-    /** Called when the activity is first created. */
+    
+	private SlidingDrawer mDrawer;
+	private Resources mResources;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mResources = getResources();
+        
+        mDrawer = (SlidingDrawer) findViewById(R.id.quick_drawer);
+
+        mDrawer.setOnDrawerOpenListener(new OnDrawerOpenListener() {
+			
+			@Override
+			public void onDrawerOpened() {
+				ImageView button = (ImageView) findViewById(R.id.quick_drawer_button);
+				button.setImageDrawable(mResources.getDrawable(R.drawable.drawer_open));
+			}
+		});
+	
+		mDrawer.setOnDrawerCloseListener(new OnDrawerCloseListener() {
+
+			@Override
+			public void onDrawerClosed() {
+				ImageView button = (ImageView) findViewById(R.id.quick_drawer_button);
+				button.setImageDrawable(mResources.getDrawable(R.drawable.drawer_closed));
+			}
+		});
+        
     }
 }
