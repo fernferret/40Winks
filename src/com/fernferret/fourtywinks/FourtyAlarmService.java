f38fd61ba4389d20c1259682fcad15fa05021640
Eric Stokes <fernferret@gmail.com>
2011-01-28 02:03:29 -0500

    Add Service

---
+++ src/com/fernferret/fourtywinks/FourtyAlarmService.java
@@ -0,0 +1,20 @@
+package com.fernferret.fourtywinks;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FourtyAlarmService extends Service {
+
+	@Override
+	public void onCreate() {
+		// TODO: Actions to perform when service is created.
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Replace with service implementation.
+		return null;
+	}
+
+}
