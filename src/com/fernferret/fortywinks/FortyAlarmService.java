4b4d0fc9eb0cc13d1c6822eee253ac05dc13d2ec
Jimmy Theis <jimmy@jetheis.com>
2011-02-11 14:30:09 -0500

    Third round of renaming
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ src/com/fernferret/fortywinks/FortyAlarmService.java
@@ -0,0 +1,26 @@
+package com.fernferret.fortywinks;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FortyAlarmService extends Service {
+
+	@Override
+	public void onCreate() {
+		// TODO: Actions to perform when service is created.
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Replace with service implementation.
+		return null;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+}
